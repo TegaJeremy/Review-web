de061f0c13e4b5a4113bd73a7eaadc76e9d238fa
tegajeremy <oghenedemartin@gmail.com>
2024-01-23 11:26:29 +0100

    updated cors

--- config/db.js
+++ config/db.js
@@ -7,5 +7,5 @@ mongoose.connect(process.env.url).then(()=>{
     console.log('connected to database successfully')
 }).catch((error)=>{
     console.log(error.message)
-})
+    })
 
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const PORT = 1244
+const cors = require("cors")
 
 const db = require('./config/db')
 const router= require('./router/review')
+app.use(cors({origin:"*"}));
 app.use(router)
 
 
